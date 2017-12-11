d93c770efcd820c1a1a53eb915e84d4aea960be9
jebias <jebias@miners.utep.edu>
2017-12-10 23:01:49 -0700

    fixed exec
    scanf was giving me issues so I changed it to a getchar loop. I attempted to get PS1, but It was giving me issues.

--- shell/myShell.c
+++ shell/myShell.c
@@ -1,8 +1,11 @@
+#include <stdlib.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 #include<unistd.h>
 #include<stdio.h>
 #include "mytoc.h"
+
+char *output = "$ ";
 
 void freearray(char**ar){
   int i;
@@ -48,9 +51,9 @@ int compare(char * str1, char * str2){
   int i;
   int str1length = findLength(str1);
   int str2length = findLength(str2);
-  if(str1length != str2length)
-    return 1;
-  for(i = 0; str1[i] != 0; i++)
+  //if(str1length != str2length)
+  //   return 1;
+  for(i = 0; str2[i] != 0; i++)
     if(str1[i] != str2[i])
       return 1;
   return 0;
@@ -66,7 +69,7 @@ int arrayHas(char* str, char c){
   return 0;
 }
 
-int execMethod(int argc, char ** argv, char ** envp){
+int execMethod(char* argc, char ** argv, char ** envp){
   /*
   //find number of pipes
   for(j = 0; list[j] != 0; j++){
@@ -87,26 +90,68 @@ int execMethod(int argc, char ** argv, char ** envp){
 	}
   }
   */
-  int retVal = execve(argc,&argv[0],envp);
+  int retVal = execve(argv[0],&argv[0],envp);
   printf("Program terminated with exit code %d\n", retVal);
   return 0;
 }
 
+int countWords(char**list){
+  int i;
+  for(i = 0; list[i] != 0; i++);
+  return i;
+}
+/*
+char** getPath(char**en){
+  int key;
+  char **path;
+  char **temp;  
+  for(key = 0; compare(en[key], "PATH\0") == 0; key++ );
+  temp = mytoc(en[key], '=');
+  path = mytoc(temp[1], ':');
+  for(key = 0; compare(en[key], "PS1\0") == 0; key++);
+  int len = countWords(temp);
+  output = (char*)malloc(sizeof(char)*(findLength(temp[1]+1)));
+  for(key = 0; key < findLength(temp[1]); key++)
+    output[key] = temp[1][key];
+  output[key] = 0;
+  
+  temp[0] = 0;
+
+  return path;
+}
+*/
 int main(int argc, char**argv, char**en){
   char* command, *param[127];
   char * cmd;
+   char str[1024];
   
   while(1){
+    // printf("$ ");
+    printf("%s", output);
+    char c = getchar();
+    int charCount = 0;
+    str[charCount] = c;
+    charCount++;
+    while(c >= 32){
+      c = getchar();
+      str[charCount] = c;
+      charCount++;
+    }
     
-    printf("$ ");
-    char str[1024];
-    scanf("%s", str);
+      //scanf("%s", str);
     //fgets(str, 1000, stdin);
-    //printf("str = %s", str);
+    //printf("str = %s\n", str);
     char** list = mytoc(str, ' ');
     int i;
     int j;
-    int numOfPipes = 0;
+    //int lastCharIsSpace = 0;
+    //for(i = 0; str[i] != 0; i++);
+    //printf("str[i-2] = %c\n",str[i-2]);
+    //if(str[i-2] == ' '){
+      
+    // lastCharIsSpace = 1;
+    // }
+    //    int numOfPipes = 0;
     //pipeFds[2];
     //pipe(pipeFds);
 
@@ -116,11 +161,23 @@ int main(int argc, char**argv, char**en){
 	if(changed == 0)
 	  printf("Error: could not change directory.\n");
       }
-      
-      for(i = 0; list[i] != 0; i++)
+      //printf("%s\n",list[1]);
+       
+      for(i = 0; list[i] >= 32; i++){
+	//	printf("i = %d\n", i);
 	param[i] = copystr(list[i]);
-      param[i]=0;
+      }
+      param[i-1] = 0;
+      //printf("lastchar is = %d\n",lastCharIsSpace);
+      /* if(lastCharIsSpace == 1){
+	printf("im here");
+	param[i-1] = 0;
+	printf("is 1\n");
+	}*/
       
+      //  printList(param);
+      // for(i = 0; list[i] != 0; i++)
+      //	printf("param[%d] = %s\n", i, param[i]);
       // printf("number of pipes = %d\n",numOfPipes);
       if(compare(param[0], "exit") == 0)
 	break;
